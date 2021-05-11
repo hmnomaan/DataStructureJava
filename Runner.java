98b00c95128f5722ca25fb3c7474094428741de0
HM Nomaan <58332260+hmnomaan@users.noreply.github.com>
2021-05-11 23:51:46 +0600

    Add files via upload

---
+++ Runner.java
@@ -0,0 +1,52 @@
+
+
+public class Runner {
+
+	public static void main(String[] args) {
+		LinkedList list=new LinkedList();
+
+//System.out.println("Head Of Queue is : "+list.element());
+/*list.add(12);
+list.add(112);
+list.add(40);
+System.out.println("Head Of Queue is : "+list.element());
+
+for(Integer Value : list)
+{
+	System.out.println(Value);
+}*/
+		
+		//list.insert(12);
+	 //list.insert(22);
+		list.insert(7100);
+		list.insert(44);
+		list.insert(424);
+		list.insert(1434);
+		list.insert(442);
+		list.insert(4234);
+		list.insert(1444);
+		list.insert(4421);
+		list.insert(4214);
+		list.insert(144);
+		list.insert(4410);
+		list.insert(42542);
+		list.insert(1454);
+		list.insert(446);
+		list.insert(4274);
+		list.insert(1844);
+		list.insert(007);
+		list.insert(4924);
+		list.insert(1474);
+		list.insertAtStart(80085);
+	   // list.insertAt(2,99900999);
+		list.insertAt(4,99900999);
+		list.deleteAt(3);
+		list.show();
+		//for(Integer value: list ) {
+			//System.out.println(value);
+		//}
+		
+	
+	}
+
+}
